72d29f7aa3c468f5895b3b825262b5377c2a2c66
klausjaspi <klaus.jaspi@gmail.com>
2020-12-30 17:14:57 +0200

    added sign up form

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import '../../src/App.css';
+import Cards from '../components/Cards';
+import Footer from '../components/Footer';
 
 export default function  Products(){
-    return <h1 className='products'>PRODUCTS</h1>;
+    return (
+    <>
+    <h1 className='products'>PRODUCTS</h1>
+        <Cards />
+        <Footer />
+    </>
+    );
 }
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import '../../src/App.css';
+import Cards from '../components/Cards';
+import Footer from '../components/Footer';
 
 export default function Services(){
-    return <h1 className="services">SERVICES</h1>;
+    return (
+        <>
+        <h1 className='services'>SERVICES</h1>
+            <Cards />
+            <Footer />
+        </>
+        );
 }
