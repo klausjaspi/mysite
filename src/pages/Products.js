b9b12fad9f2fd16f33cdb0a3dc5e123c3e817c90
klausjaspi <klaus.jaspi@gmail.com>
2021-01-08 15:53:42 +0200

    Product details & Adjusted Slider

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import '../../src/App.css';
 import Footer from '../components/Footer';
-import img from "../images/logo2.png"
-import img2 from "../images/workfromhome.jpg";
+import img from "../images/yougot.jpg"
+import img2 from "../images/speak.jpg";
+import img3 from "../images/roadtrip.jpg"
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -12,11 +13,14 @@ export default function  Products(){
     const settings = {
         dots: true,
         infinite: true,
-        speed: 500,
+        speed: 1200,
         slidesToShow: 1,
         slidesToScroll: 1,
         centerMode: true,
         arrows: true,
+        autoplay: true,
+        autoplaySpeed: 5500,
+        pauseOnHover: true,
         responsive: [
             {
               breakpoint: 700,
@@ -45,13 +49,18 @@ export default function  Products(){
       const products = [
         {
           img: img,
-          title: 'DolorEE magna',
-          text: 'Lorem ipsum dolor sit amet elit.'
+          title: "Coaching   -  Have you hit dead-end and lack motivation?",
+          text: "\nWe have the best coaches from almost every field around the world, learn more!"
         },
         {
           img: img2,
-          title: 'Eget est lorem',
-          text: 'Lorem Ipsum adipiscing elit ipsum.'
+          title: 'Course   -  How to give a unforgettable speech',
+          text: '\nOur award winning speakers unveil you the secrets of how to give an amazing speech. \n\nSign up today, limited seats!'
+        },
+        {
+          img: img3,
+          title: "Guide   -  Must visit road trip places",
+          text: "\nReady to go guide with trip plans and details around the world."
         }
       ]
 
@@ -66,7 +75,7 @@ export default function  Products(){
                     <div key={i} className="img-card">
                         <img src={x.img} className="img" alt=""/>
                         <div class="card-body">
-                            <div className="cardtitle">{x.title}</div>
+                            <div className="card-title">{x.title}</div>
                             <div className="card-text">{x.text}</div>
                         </div>
                             
