465dc6bc1eae4f2bd6677e15fb29b167dcff715b
klausjaspi <klaus.jaspi@gmail.com>
2021-09-03 10:32:29 +0300

    Updated about me section

--- src/pages/About.js
+++ src/pages/About.js
@@ -35,8 +35,11 @@ function About() {
                     
                     <div className="introtext">
                         <h3>Get To Know Me</h3>
-                        <p>Born in Turku, currently living in Naantali with my Wife and two cats. I'm few months away 26 years old.{"\n"}I'm into programming, hiking, and playing guitar. I'm cheerful, open-minded, self-motivated, and honest personality. It's easy to get along with me. I excel working as a team member, but also on my own.{"\n\n"}</p>
-                        <p>I'm on my second yeard of ICT-engineer studies in Turku University of Applied Sciences. My advanced special studies focus on software & project management.{"\n\n\n"}I'm enthusiastic and I see challenges as opportunities to grow from. As a committed, passionated and creative student I believe I have a lot to offer for you. I'm open to tell more about myself in person.</p>
+                        <p>I'm 26 years old, born in Turku. I'm currently living in Naantali with my Wife and our two cats.{"\n"}
+                        I'm into programming, hiking, and playing guitar. I'm cheerful, open-minded, self-motivated, and honest personality. It's easy to get along with me. I excel working as a team member, but also on my own.{"\n"}</p>
+                        <p>I'm enthusiastic and I see challenges as opportunities to grow from.{"\n\n"}</p>
+                        <p>I'm on my third year of ICT-engineer studies in Turku University of Applied Sciences. I'm proud to say that I joined Sofokus in April 2021 as a Software Developer Trainee, my aim is to continuously develop myself and my skills. 
+                        My advanced special studies focus on software & project management.</p>
                     </div>
                 
                 
@@ -56,14 +59,29 @@ function About() {
                             <p>As a part of my studies, I got to learn Wordpress and Elementor by working with two other students on a commercial project.{"\n"}Project was done in theFIRMA, which is a learning environment in Turku University of Applied Sciences that operates like a real company.</p>
                             <p>{"\n"}I got valuable experience, not only on Wordpress and Elementor, but also on project management and how to carry out a commercial project.</p>
 
+                            <p>In April 2021 my half an year trainee position at Sofokus began and I learned how to do WordPress on a professional level. 
+                                For example, I learned to do acf custom Gutenberg blocks of which I had not even heard of before. I'm excited and cant't wait to get more professional level experience on this!</p>
+                            
+                            <h4>Php</h4>
+                            <p>I had no experience on php prior joining Sofokus in April 2021. I got the hang of it quite quickly and have learned a lot since. I've used php especially in WordPress acf custom block development.{"\n"}
+                            I will continue to polish my skills on PHP in the future.</p>
+
+                            <h4>Django</h4>
+                            <p>I've had the opportunity to practice my skills on Django on a professional level in Sofokus. 
+                            I've done few my own practice projects on Django on my own.{"\n"}I'm very eager to learn more on this subject, especially on professional level.</p>
+
+
                             <h4>HTML, CSS & JavaScript</h4>
-                            <p>Before early fall of 2020 I had no experience on web development, only a deep drive to learn and master it.{"\n"}I watched many tutorials and along them began my trial & error type of learning process.{"\n"}I've grown very passionated about web development and will continue to refine my knowledge on it in future.{"\n\n"}</p>
+                            <p>Before early fall of 2020 I had no experience on web development, only a deep drive to learn and master it.{"\n"}I watched many 
+                            tutorials and along them began my trial & error type of learning process.{"\n"}I've learned a lot since the fall of 2020 and have had (and still do) the chance to work on these on a professional level.{"\n\n"}</p>
                             <a href="https://github.com/klausjaspi/Reserve_app" target="blank">My First Website: <u>https://github.com/klausjaspi/Reserve_app</u></a>
 
                             <h4>React</h4>
-                            <p>I wanted to learn more advanced web development and so end up learning React through Sololearn and tutorials.{"\n"}My first React project was a simple To-Do list website done based on a tutorial. Goal was to learn the basics and get some exprerience.{"\n\n"}</p>
+                            <p>I wanted to learn more advanced web development and so end up learning React through Sololearn and tutorials.{"\n"}
+                            My first React project was a simple To-Do list website done based on a tutorial. Goal was to learn the basics and get some exprerience.{"\n\n"}</p>
                             <a href="https://github.com/klausjaspi/Todo">Check it out: <u>https://github.com/klausjaspi/Todo</u></a>
-                            <p>{"\n"}My second and current React project is this website aka. <u>MySite</u>.{"\n"}Like before, I began with tutorial but continued to refine and add parts to it, growing it to a extend.{"\n"}My goal was to learn how to develop a modern and clean website which would also work as my portfolio.{"\n\n"}I hope you've enjoyed your stay!</p>
+                            <p>{"\n"}My second and current React project is this website aka. <a href="https://mysite-klausjaspi.vercel.app/"><u>MySite</u></a>.{"\n"}Like before, I began with tutorial but continued to refine and add parts to it, 
+                            growing it to a extend.{"\n"}My goal was to learn how to develop a modern and clean website which would also work as my portfolio.{"\n\n"}I hope you've enjoyed your stay!</p>
                         </div>
                     </div>
                 </div>
